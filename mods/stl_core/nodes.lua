89da356cb08d8accc3ef658cb5e7ad2b266b8075
theidealist <theidealistmusic@gmail.com>
2024-11-02 18:07:09 +0000

    made nodes breakable and changed palettes

--- mods/stl_core/nodes.lua
+++ mods/stl_core/nodes.lua
@@ -1,10 +1,21 @@
+--Override hand so it can break stuff
+minetest.override_item("", {
+    tool_capabilities = {
+        full_punch_interval = 1,
+        groupcaps = {
+            crumbly = {times={2}}
+        }
+    }
+})
+
 --Basic stone variations used by all planets
 for i = 1, 8 do
     minetest.register_node("stl_core:stone"..i, {
         description = "Stone "..i,
         tiles = {"stl_core_stone"..i..".png"},
         paramtype2 = "color",
-        palette = "palette_stone.png"
+        palette = "palette.png",
+        groups = {cracky=1}
     })
 end
 
@@ -14,6 +25,7 @@ for i = 1, 8 do
         description = "Filler "..i,
         tiles = {"stl_core_filler"..i..".png"},
         paramtype2 = "color",
-        palette = "palette_filler.png"
+        palette = "palette.png",
+        groups = {crumbly=1}
     })
 end
