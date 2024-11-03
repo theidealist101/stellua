8c53385db2dba9b4979c0f69e2477070401546c2
theidealist <theidealistmusic@gmail.com>
2024-11-03 21:59:57 +0000

    might be easier to just not save the planets at all

--- mods/stl_core/mapgen.lua
+++ mods/stl_core/mapgen.lua
@@ -3,13 +3,8 @@
 --Must also keep track of player properties and set them accordingly
 
 --Remember what the planet stuff was so we don't have to calculate it again
-local storage = minetest.get_mod_storage()
-local planets = minetest.deserialize(storage:get("planets")) or {}
+local planets = {}
 stellua.planets = planets
-
-local function save_planets()
-    storage:set_string("planets", minetest.serialize(planets))
-end
 
 --Choose a certain number of items from a range randomly
 local function choices(rand, n, a, b)
@@ -61,187 +56,186 @@ local SKY_COL = vector.new(97, 181, 245)
 
 --Set up planets and attributes on first load
 minetest.register_on_mods_loaded(function()
-    if not next(planets) then
-        local rand = PcgRandom(minetest.get_mapgen_setting("seed"))
-        for i = 1, 60 do
+    local rand = PcgRandom(minetest.get_mapgen_setting("seed"))
+    for i = 1, 60 do
 
-            --some basics
-            local seed = rand:next()
-            local prand = PcgRandom(seed)
-            local planet = {}
-            table.insert(planets, planet)
-            planet.name = stellua.generate_name(prand, "star")
-            planet.seed = seed
-            local level = stellua.get_planet_level(i)
-            planet.level = level
-            planet.heat_stat = prand:next(100, 300)+prand:next(0, 200) --temperature in Kelvin
-            planet.atmo_stat = prand:next(0, 300)*0.01 --atmospheric pressure in atmospheres
-            planet.life_stat = planet.heat_stat <= 400 and planet.heat_stat >= 200 and planet.atmo_stat > 0.5 and prand:next(1, 200)*0.01 or 0
+        --some basics
+        local seed = rand:next()
+        local prand = PcgRandom(seed)
+        local planet = {}
+        table.insert(planets, planet)
+        planet.name = stellua.generate_name(prand, "star")
+        planet.seed = seed
+        local level = stellua.get_planet_level(i)
+        planet.level = level
+        planet.heat_stat = prand:next(100, 300)+prand:next(0, 200) --temperature in Kelvin
+        planet.atmo_stat = prand:next(0, 300)*0.01 --atmospheric pressure in atmospheres
+        planet.life_stat = planet.heat_stat <= 400 and planet.heat_stat >= 200 and planet.atmo_stat > 0.5 and prand:next(1, 200)*0.01 or 0
 
-            --sky stuffs
-            local alpha = math.min(planet.atmo_stat, 1)
-            local fog = planet.atmo_stat*0.33333
-            local fog_dist = 250-fog*180
-            local fog_table = {fog_distance=fog_dist, fog_start=math.max(1-50/fog_dist, 0)}
-            local b = luamap.remap(planet.heat_stat, 100, 500, 255, 0)
-            local total = prand:next(255, 384)-b
-            local r = prand:next(0, total)
-            local col = SKY_COL*alpha*(1-fog)+vector.new(math.min(r, 255), math.min(total-r, 255), b)*fog
-            function planet.sky(timeofday)
-                local newcol = col*math.min(math.max(luamap.remap(timeofday < 0.5 and timeofday or 1-timeofday, 0.19, 0.23, 0.2, 1), 0.2), 1)
-                return {
-                    type = "plain",
-                    base_color = {r=newcol.x, g=newcol.y, b=newcol.z},
-                    fog = fog_table,
-                    clouds = false --clouds are currently bugged
-                }
+        --sky stuffs
+        local alpha = math.min(planet.atmo_stat, 1)
+        local fog = planet.atmo_stat*0.33333
+        local fog_dist = 250-fog*180
+        local fog_table = {fog_distance=fog_dist, fog_start=math.max(1-50/fog_dist, 0)}
+        local b = luamap.remap(planet.heat_stat, 100, 500, 255, 0)
+        local total = prand:next(255, 384)-b
+        local r = prand:next(0, total)
+        local col = SKY_COL*alpha*(1-fog)+vector.new(math.min(r, 255), math.min(total-r, 255), b)*fog
+        planet.stars = {day_opacity=1-alpha}
+
+        function planet.sky(timeofday)
+            local newcol = col*math.min(math.max(luamap.remap(timeofday < 0.5 and timeofday or 1-timeofday, 0.19, 0.23, 0.2, 1), 0.2), 1)
+            return {
+                type = "plain",
+                base_color = {r=newcol.x, g=newcol.y, b=newcol.z},
+                fog = fog_table,
+                clouds = false --clouds are currently bugged
+            }
+        end
+
+        --noise maps
+        local scale = prand:next(100, 200)*0.01
+        local spread = math.round(prand:next(100, 200)*scale)
+        luamap.register_noise("planet"..i, {
+            type = "2d",
+            ymin = level-500,
+            ymax = level+499,
+            np_vals = {
+                offset = level,
+                scale = 10^scale,
+                spread = {x=spread, y=spread, z=spread},
+                seed = seed,
+                octaves = math.round(3+scale),
+                persistence = 0.5,
+                lacunarity = 2
+            }
+        })
+
+        --specifics of terrain
+        planet.mapgen_stone = "stl_core:stone"..prand:next(1, 8)
+        planet.c_stone = minetest.get_content_id(planet.mapgen_stone)
+        planet.param2_stone = get_heat_param2(prand, planet.heat_stat)
+
+        local a, b, c = prand:next(1, 8)
+        planet.mapgen_filler = "stl_core:filler"..a
+        planet.c_filler = minetest.get_content_id(planet.mapgen_filler)
+        planet.param2_filler = get_nearby_param2(prand, planet.param2_stone)
+        planet.depth_filler = math.ceil(planet.life_stat+prand:next(0, 100)*0.01)
+
+        local water_options = {}
+        for _ = 1, 10 do table.insert(water_options, {0, 0}) end
+        for _, val in pairs(stellua.registered_waters) do
+            local name, defs = unpack(val)
+            if planet.heat_stat < defs.boil_point and (defs.frozen_tiles or planet.heat_stat > defs.melt_point) then
+                for _ = 1, (defs.weight or 1)*10 do
+                    table.insert(water_options, {name, defs})
+                end
             end
-            planet.stars = {day_opacity=1-alpha}
+        end
 
-            --noise maps
-            local scale = prand:next(100, 200)*0.01
-            local spread = math.round(prand:next(100, 200)*scale)
-            luamap.register_noise("planet"..i, {
-                type = "2d",
-                ymin = level-500,
-                ymax = level+499,
-                np_vals = {
-                    offset = level,
-                    scale = 10^scale,
-                    spread = {x=spread, y=spread, z=spread},
-                    seed = seed,
-                    octaves = math.round(3+scale),
-                    persistence = 0.5,
-                    lacunarity = 2
-                }
+        if planet.atmo_stat >= 0.5 and #water_options > 0 then
+            local water, defs = unpack(water_options[prand:next(1, #water_options)])
+            if water ~= 0 then
+                planet.water_level = level+prand:next(math.round(-0.5*10^scale), math.round(0.5*10^scale))
+
+                planet.mapgen_water = water.."_source"
+                planet.c_water = minetest.get_content_id(planet.mapgen_water)
+                planet.water_name = defs.description
+
+                repeat b = prand:next(1, 8) until a ~= b
+                planet.mapgen_seabed = "stl_core:filler"..b
+                planet.c_seabed = minetest.get_content_id(planet.mapgen_seabed)
+                planet.param2_seabed = get_nearby_param2(prand, planet.param2_stone)
+                planet.depth_seabed = math.ceil(planet.life_stat+prand:next(-100, 0)*0.01)
+
+                repeat c = prand:next(1, 8) until a ~= c and b ~= c
+                planet.mapgen_beach = "stl_core:filler"..c
+                planet.c_beach = minetest.get_content_id(planet.mapgen_beach)
+                planet.param2_beach = get_nearby_param2(prand, planet.param2_stone-32, 2)
+                planet.depth_beach = math.ceil(planet.life_stat+prand:next(-50, 50)*0.01)
+
+                if planet.heat_stat <= defs.melt_point and defs.frozen_tiles then
+                    planet.mapgen_water_top = water.."_frozen"
+                    planet.c_water_top = minetest.get_content_id(planet.mapgen_water_top)
+                    planet.depth_water_top = math.ceil((defs.melt_point-planet.heat_stat+1)*0.1)
+                    planet.water_name = planet.water_name.." Ice"
+
+                    if defs.snow then
+                        local snow_defs = minetest.registered_nodes[defs.snow]
+                        minetest.register_decoration({
+                            deco_type = "simple",
+                            place_on = {planet.mapgen_stone, planet.mapgen_filler, planet.mapgen_beach},
+                            fill_ratio = math.min(snow_defs.melt_point-planet.heat_stat+1, planet.heat_stat-snow_defs.start_point+1)*0.01+0.5,
+                            y_min = level-500,
+                            y_max = level+499,
+                            decoration = defs.snow,
+                            param2 = 8,
+                            param2_max = 16
+                        })
+                    end
+                end
+            end
+        end
+
+        local snow_options = {{0, 0}}
+        for _ = 1, 20 do table.insert(snow_options, {0, 0}) end
+        for _, val in pairs(stellua.registered_snows) do
+            local name, defs = unpack(val)
+            if planet.heat_stat < defs.melt_point and planet.heat_stat > defs.start_point then
+                for _ = 1, (defs.weight or 1)*10 do
+                    table.insert(snow_options, {name, defs})
+                end
+            end
+        end
+
+        if #snow_options > 0 then
+            local snow, defs = unpack(snow_options[prand:next(1, #snow_options)])
+            if snow ~= 0 then
+                minetest.register_decoration({
+                    deco_type = "simple",
+                    place_on = {planet.mapgen_stone, planet.mapgen_filler, planet.mapgen_beach},
+                    fill_ratio = math.min(defs.melt_point-planet.heat_stat+1, planet.heat_stat-defs.start_point+1)*0.01+0.5,
+                    y_min = level-500,
+                    y_max = level+499,
+                    decoration = snow,
+                    param2 = 8,
+                    param2_max = 16
+                })
+            end
+        end
+
+        --foliage
+        if planet.life_stat > 0 then
+            local fill_ratio = planet.life_stat*0.2+prand:next(1, 12)*0.005
+            local param2_grass = get_nearby_param2(prand, planet.param2_filler)
+            minetest.register_decoration({
+                deco_type = "simple",
+                place_on = {planet.mapgen_filler},
+                fill_ratio = fill_ratio,
+                y_min = level-500,
+                y_max = level+499,
+                decoration = "stl_core:grass"..prand:next(1, 8),
+                param2 = param2_grass
             })
-
-            --specifics of terrain
-            planet.mapgen_stone = "stl_core:stone"..prand:next(1, 8)
-            planet.c_stone = minetest.get_content_id(planet.mapgen_stone)
-            planet.param2_stone = get_heat_param2(prand, planet.heat_stat)
-
-            local a, b, c = prand:next(1, 8)
-            planet.mapgen_filler = "stl_core:filler"..a
-            planet.c_filler = minetest.get_content_id(planet.mapgen_filler)
-            planet.param2_filler = get_nearby_param2(prand, planet.param2_stone)
-            planet.depth_filler = math.ceil(planet.life_stat+prand:next(0, 100)*0.01)
-
-            local water_options = {}
-            for _ = 1, 10 do table.insert(water_options, {0, 0}) end
-            for _, val in pairs(stellua.registered_waters) do
-                local name, defs = unpack(val)
-                if planet.heat_stat < defs.boil_point and (defs.frozen_tiles or planet.heat_stat > defs.melt_point) then
-                    for _ = 1, (defs.weight or 1)*10 do
-                        table.insert(water_options, {name, defs})
-                    end
-                end
-            end
-
-            if planet.atmo_stat >= 0.5 and #water_options > 0 then
-                local water, defs = unpack(water_options[prand:next(1, #water_options)])
-                if water ~= 0 then
-                    planet.water_level = level+prand:next(math.round(-0.5*10^scale), math.round(0.5*10^scale))
-
-                    planet.mapgen_water = water.."_source"
-                    planet.c_water = minetest.get_content_id(planet.mapgen_water)
-                    planet.water_name = defs.description
-
-                    repeat b = prand:next(1, 8) until a ~= b
-                    planet.mapgen_seabed = "stl_core:filler"..b
-                    planet.c_seabed = minetest.get_content_id(planet.mapgen_seabed)
-                    planet.param2_seabed = get_nearby_param2(prand, planet.param2_stone)
-                    planet.depth_seabed = math.ceil(planet.life_stat+prand:next(-100, 0)*0.01)
-
-                    repeat c = prand:next(1, 8) until a ~= c and b ~= c
-                    planet.mapgen_beach = "stl_core:filler"..c
-                    planet.c_beach = minetest.get_content_id(planet.mapgen_beach)
-                    planet.param2_beach = get_nearby_param2(prand, planet.param2_stone-32, 2)
-                    planet.depth_beach = math.ceil(planet.life_stat+prand:next(-50, 50)*0.01)
-
-                    if planet.heat_stat <= defs.melt_point and defs.frozen_tiles then
-                        planet.mapgen_water_top = water.."_frozen"
-                        planet.c_water_top = minetest.get_content_id(planet.mapgen_water_top)
-                        planet.depth_water_top = math.ceil((defs.melt_point-planet.heat_stat+1)*0.1)
-                        planet.water_name = planet.water_name.." Ice"
-
-                        if defs.snow then
-                            local snow_defs = minetest.registered_nodes[defs.snow]
-                            minetest.register_decoration({
-                                deco_type = "simple",
-                                place_on = {planet.mapgen_stone, planet.mapgen_filler, planet.mapgen_beach},
-                                fill_ratio = math.min(snow_defs.melt_point-planet.heat_stat+1, planet.heat_stat-snow_defs.start_point+1)*0.01+0.5,
-                                y_min = level-500,
-                                y_max = level+499,
-                                decoration = defs.snow,
-                                param2 = 8,
-                                param2_max = 16
-                            })
-                        end
-                    end
-                end
-            end
-
-            local snow_options = {{0, 0}}
-            for _ = 1, 20 do table.insert(snow_options, {0, 0}) end
-            for _, val in pairs(stellua.registered_snows) do
-                local name, defs = unpack(val)
-                if planet.heat_stat < defs.melt_point and planet.heat_stat > defs.start_point then
-                    for _ = 1, (defs.weight or 1)*10 do
-                        table.insert(snow_options, {name, defs})
-                    end
-                end
-            end
-
-            if #snow_options > 0 then
-                local snow, defs = unpack(snow_options[prand:next(1, #snow_options)])
-                if snow ~= 0 then
-                    minetest.register_decoration({
-                        deco_type = "simple",
-                        place_on = {planet.mapgen_stone, planet.mapgen_filler, planet.mapgen_beach},
-                        fill_ratio = math.min(defs.melt_point-planet.heat_stat+1, planet.heat_stat-defs.start_point+1)*0.01+0.5,
-                        y_min = level-500,
-                        y_max = level+499,
-                        decoration = snow,
-                        param2 = 8,
-                        param2_max = 16
-                    })
-                end
-            end
-
-            --foliage
-            if planet.life_stat > 0 then
-                local fill_ratio = planet.life_stat*0.2+prand:next(1, 12)*0.005
-                local param2_grass = get_nearby_param2(prand, planet.param2_filler)
+            minetest.register_decoration({
+                deco_type = "simple",
+                place_on = {planet.mapgen_stone, planet.mapgen_filler},
+                fill_ratio = fill_ratio*prand:next(1, 10)*0.1,
+                y_min = level-500,
+                y_max = level+499,
+                decoration = "stl_core:grass"..prand:next(1, 8),
+                param2 = get_nearby_param2(prand, param2_grass, 2)
+            })
+            if planet.life_stat > 1 then
                 minetest.register_decoration({
                     deco_type = "simple",
                     place_on = {planet.mapgen_filler},
-                    fill_ratio = fill_ratio,
+                    fill_ratio = (planet.life_stat-1)*0.4+prand:next(1, 20)*0.1,
                     y_min = level-500,
                     y_max = level+499,
-                    decoration = "stl_core:grass"..prand:next(1, 8),
-                    param2 = param2_grass
+                    decoration = "stl_core:moss",
+                    param2 = get_nearby_param2(prand, param2_grass)
                 })
-                minetest.register_decoration({
-                    deco_type = "simple",
-                    place_on = {planet.mapgen_stone, planet.mapgen_filler},
-                    fill_ratio = fill_ratio*prand:next(1, 10)*0.1,
-                    y_min = level-500,
-                    y_max = level+499,
-                    decoration = "stl_core:grass"..prand:next(1, 8),
-                    param2 = get_nearby_param2(prand, param2_grass, 2)
-                })
-                if planet.life_stat > 1 then
-                    minetest.register_decoration({
-                        deco_type = "simple",
-                        place_on = {planet.mapgen_filler},
-                        fill_ratio = (planet.life_stat-1)*0.4+prand:next(1, 20)*0.1,
-                        y_min = level-500,
-                        y_max = level+499,
-                        decoration = "stl_core:moss",
-                        param2 = get_nearby_param2(prand, param2_grass)
-                    })
-                end
             end
         end
     end
