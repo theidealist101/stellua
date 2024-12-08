59ab2334db6a4757ce55a030317840243ea89509
theidealist <theidealistmusic@gmail.com>
2024-12-08 17:13:40 +0000

    added dye which can be used on certain nodes

---
+++ mods/stl_decor/dyes.lua
@@ -0,0 +1,45 @@
+minetest.register_node("stl_decor:flower", {
+    description = "Flower",
+    drawtype = "plantlike",
+    tiles = {"stl_decor_flower.png"},
+    inventory_image = "stl_decor_flower.png",
+    selection_box = {type="fixed", fixed={-0.1875, -0.5, -0.1875, 0.1875, 0.1875, 0.1875}},
+    walkable = true,
+    paramtype = "light",
+    sunlight_propagates = true,
+    paramtype2 = "color",
+    palette = "palette_dye.png",
+    groups = {snappy=2, attached_node=1},
+    sounds = stellua.node_sound_defaults()
+})
+
+minetest.register_craftitem("stl_decor:dye", {
+    description = "Dye",
+    inventory_image = "dye_white.png",
+    palette = "palette_dye.png"
+})
+
+minetest.register_craft({
+    type = "shapeless",
+    output = "stl_decor:dye 4",
+    recipe = {"stl_decor:flower"}
+})
+
+stellua.register_color_craft("stl_decor:dye", "stl_decor:flower")
+
+function stellua.register_dyed_node(name, node, desc)
+    local defs = table.copy(minetest.registered_nodes[node])
+    defs.description = desc
+    defs.paramtype2 = "color"
+    defs.palette = "palette_dye.png"
+    minetest.register_node(name, defs)
+    minetest.register_craft({
+        type = "shapeless",
+        output = name,
+        recipe = {node, "stl_decor:dye"}
+    })
+    stellua.register_color_craft(name, "stl_decor:dye")
+end
+
+stellua.register_dyed_node("stl_decor:stained_glass", "stl_decor:glass", "Stained Glass")
+stellua.register_dyed_node("stl_decor:stained_wood", "stl_core:wood", "Stained Wood")
---
+++ mods/stl_decor/nodes.lua
@@ -0,0 +1,58 @@
+minetest.register_node("stl_decor:stone_brick", {
+    description = "Stone Brick",
+    tiles = {"stl_decor_stone_brick.png"},
+    paramtype2 = "color",
+    palette = "palette.png",
+    groups = {cracky=2},
+    sounds = stellua.node_sound_stone_defaults()
+})
+
+minetest.register_craft({
+    output = "stl_decor:stone_brick 4",
+    recipe = {
+        {"stl_core:cobble", "stl_core:cobble"},
+        {"stl_core:cobble", "stl_core:cobble"}
+    }
+})
+
+stellua.register_color_craft("stl_decor:stone_brick", "stl_core:cobble")
+
+minetest.register_node("stl_decor:filler_brick", {
+    description = "Earth Brick",
+    tiles = {"stl_decor_filler_brick.png"},
+    paramtype2 = "color",
+    palette = "palette.png",
+    groups = {cracky=1},
+    sounds = stellua.node_sound_stone_defaults()
+})
+
+for i = 1, 8 do
+    minetest.register_craft({
+        output = "stl_decor:filler_brick 4",
+        recipe = {
+            {"stl_core:filler"..i, "stl_core:filler"..i},
+            {"stl_core:filler"..i, "stl_core:filler"..i}
+        }
+    })
+    stellua.register_color_craft("stl_decor:filler_brick", "stl_core:filler"..i)
+end
+
+minetest.register_node("stl_decor:glass", {
+    description = "Glass",
+    drawtype = "glasslike",
+    tiles = {"default_glass.png"},
+    paramtype = "light",
+    sunlight_propagates = true,
+    groups = {cracky=1, spaceship=1},
+    sounds = stellua.node_sound_glass_defaults()
+})
+
+for i = 1, 5 do
+    minetest.register_craft({
+        output = "stl_decor:glass",
+        recipe = {
+            {"stl_core:quartz"..i, "stl_core:quartz"..i},
+            {"stl_core:quartz"..i, "stl_core:quartz"..i}
+        }
+    })
+end
